46385386aa9d59f3584b89bf62136d313d80c4a3
Sicco van Sas <siccovansas@users.noreply.github.com>
2019-05-22 18:25:10 +0200

    Add fabfile

---
+++ fabfile.py
@@ -0,0 +1,15 @@
+from fabric.api import run, env, cd, sudo
+
+env.use_ssh_config = True
+env.hosts = ["Oxygen"]
+
+# Container used to compile the assets
+NODE_CONTAINER = 'accountabilityhacktheme2019_node_1'
+
+def deploy():
+    with cd('/home/projects/wordpress-new/wp-content/themes/accountability-hack-theme-2019'):
+        sudo('git pull git@github.com:openstate/accountability-hack-theme-2019.git')
+        output = sudo('docker inspect --format="{{.State.Status}}" %s' % (NODE_CONTAINER))
+        if output != 'running':
+            exit('\n*** ERROR: The %s container, used to compile the assets, is not running. Please build/run/start the container.' % (NODE_CONTAINER))
+        sudo('docker exec %s yarn build' % (NODE_CONTAINER))
