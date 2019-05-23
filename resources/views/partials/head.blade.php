24031dd7833b861689f5bbf555fd186f1b494b2c
Sicco van Sas <siccovansas@users.noreply.github.com>
2019-05-23 18:47:30 +0200

    Add Open Graph logo and analytics

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,5 +2,21 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="author" content="Open State Foundation">
+  <meta property="og:title" content="Accountability Hack" />
+  <meta property="og:type" content="website" />
+  <meta property="og:image" content="@asset('images/logo-og.png')" />
+  <meta property="og:image:secure_url" content="@asset('images/logo-og.png')" />
+  <meta property="og:image:type" content="image/png" />
+  <meta property="og:image:width" content="1500" />
+  <meta property="og:image:height" content="500" />
   @php wp_head() @endphp
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-32274817-9"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-32274817-9', { 'anonymize_ip': true });
+  </script>
 </head>
