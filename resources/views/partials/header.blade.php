49dc505eb6c7f86e915d0f619169dd1ae1f37280
Sicco van Sas <siccovansas@users.noreply.github.com>
2019-05-28 18:50:12 +0200

    Fix menu responsiveness

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,15 +5,13 @@
         <img class="navbar-brand-img img-fluid" src="@asset('images/ACC-HACK-Logo-Sticky-fixed.svg')">
       </a>
 
-      <div class="justify-content-end">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <h1 class="menu">menu</h1>
-        </button>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <h1 class="menu">menu</h1>
+      </button>
 
-        @if (has_nav_menu('primary_navigation'))
-          {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav', 'container' => 'div', 'container_id' => 'navbarSupportedContent', 'container_class' => 'collapse navbar-collapse', 'walker' => new App\wp_bootstrap4_navwalker()]) !!}
-        @endif
-      </div>
+      @if (has_nav_menu('primary_navigation'))
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav', 'container' => 'div', 'container_id' => 'navbarSupportedContent', 'container_class' => 'collapse navbar-collapse', 'walker' => new App\wp_bootstrap4_navwalker()]) !!}
+      @endif
     </nav>
   </div>
 </header>
