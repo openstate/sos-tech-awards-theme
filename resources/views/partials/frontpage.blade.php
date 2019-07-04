299809995569a1afd218fa0b7299f7f0e10779ad
Sicco van Sas <siccovansas@users.noreply.github.com>
2019-07-04 18:48:53 +0200

    Add side-events button

--- resources/views/partials/frontpage.blade.php
+++ resources/views/partials/frontpage.blade.php
@@ -20,6 +20,24 @@
       </div>
     </div>
   </div>
+
+  <div class="row bg-white">
+    <div class="col-12">
+      <div class="row call-to-action bg-lichtblauw">
+        <div class="col-sm-4">
+          <i class="far fa-calendar-plus fa-2x"></i> &nbsp;<h1 class="d-inline">04 OKT</h1>
+        </div>
+        <div class="col-sm-4 text-center">
+          <h1>Side-events</h1>
+        </div>
+        <div class="col-sm-4 text-right">
+          <a href="https://www.eventbrite.nl/e/tickets-side-events-accountability-hack-64720296148" class="red-button btn btn-primary" target="_blank" rel="noopener">
+            <h3 class="d-inline">meld je aan</h3> &nbsp;<i class="fas fa-long-arrow-alt-right"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
   
   <div class="row bg-white">
     <div class="col-sm-8">
